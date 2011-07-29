5148005f7301c4a8fa841e266cff296802127895
Jordan Kohl <kohl.jordan@gmail.com>
2011-07-29 14:04:29 -0500

    what

--- application/models/tag_model.php
+++ application/models/tag_model.php
@@ -7,12 +7,7 @@ class Tag_model extends Model {
 		parent::__construct();
 		
 		// Tables being used:
-		//$this->_table_project = 		"project";
-		//$this->_table_project_tag =		"project_tag";
-		$this->_table_tag =		"tags";
-		
-		//$this->load->model('tag_model','_tag');
-		
+		$this->_table_tag =		"tags";		
 	}
 	
 	function getTags($array)
